d996c8f6d625f150c4ff595edcf3399d8ebeb29e
Victor <victor.cifuentes@alumnos.usm.cl>
2014-05-26 12:08:06 -0400

    Cambios en el servidor

---
+++ ServerApplication/src/serverapplication/Protocolo.java
@@ -0,0 +1,162 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package serverapplication;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Hashtable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Victor
+ */
+public class Protocolo {
+    private String nick;
+    private ClientConn conn;
+ 
+    /* a hash table from user nicks to the corresponding connections */
+    private static Hashtable<String, ClientConn> nicks = 
+        new Hashtable<String, ClientConn>();
+ 
+    private static final String msg_OK = "MENSAJE ENVIADO";
+    private static final String msg_FILE_OK = "ARCHIVO COMPARTIDO";
+    private static final String msg_NICK_IN_USE = "NICK IN USE";
+    private static final String msg_SPECIFY_NICK = "SPECIFY NICK";
+    private static final String msg_INVALID = "COMANDO INVALIDO";
+    private static final String msg_SEND_FAILED = "NO SE ENCONTRÓ AL USUARIO CON EL NICK INGRESADO.";
+ 
+    /**
+     * Adds a nick to the hash table 
+     * returns false if the nick is already in the table, true otherwise
+     */
+    private static boolean add_nick(String nick, ClientConn c) {
+        if (nicks.containsKey(nick)) {
+            return false;
+        } else {
+            nicks.put(nick, c);
+            return true;
+        }
+    }
+ 
+    public Protocolo(ClientConn c) {
+        nick = null;
+        conn = c;
+    }
+ 
+    private void log(String msg) {
+        System.err.println(msg);
+    }
+ 
+    public boolean isAuthenticated() {
+        return ! (nick == null);
+    }
+ 
+    /**
+     * Implements the authentication protocol.
+     * This consists of checking that the message starts with the NICK command
+     * and that the nick following it is not already in use.
+     * returns: 
+     *  msg_OK if authenticated
+     *  msg_NICK_IN_USE if the specified nick is already in use
+     *  msg_SPECIFY_NICK if the message does not start with the NICK command 
+     */
+    private String authenticate(String msg) {
+        if(msg.startsWith("NICK")) {
+            String tryNick = msg.substring(5);
+            if(add_nick(tryNick, this.conn)) {
+                log("Nick " + tryNick + " ha entrado a la sala de chat.");
+                this.nick = tryNick;
+                return msg_OK;
+            } else {
+                return msg_NICK_IN_USE;
+            }
+        } else {
+            return msg_SPECIFY_NICK;
+        }
+    }
+ 
+    /**
+     * Send a message to another user.
+     * @recepient contains the recepient's nick
+     * @msg contains the message to send
+     * return true if the nick is registered in the hash, false otherwise
+     */
+    private boolean sendMsg(String recipient, String msg) {
+        if (nicks.containsKey(recipient)) {
+            ClientConn c = nicks.get(recipient);
+            c.sendMsg(nick + " escribió: " + msg);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    private boolean sendFile(String receptor, String nombreArchivo){
+        if(nicks.containsKey(receptor)){
+            ClientConn c = nicks.get(receptor);
+            FileInputStream fis = null;
+            BufferedInputStream bis = null;
+            OutputStream os = null;
+            File archivo = new File(nombreArchivo);
+            byte [] mybytearray  = new byte [(int)archivo.length()];
+            try{
+                fis = new FileInputStream(archivo);
+                bis = new BufferedInputStream(fis);
+                bis.read(mybytearray,0,mybytearray.length);
+                
+                
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(Protocolo.class.getName()).log(Level.SEVERE, null, ex);
+                return false;
+            } catch (IOException ex) {
+                Logger.getLogger(Protocolo.class.getName()).log(Level.SEVERE, null, ex);
+                return false;
+            }
+            return true;
+        }
+        else return false;
+    }
+ 
+    /**
+     * Process a message coming from the client
+     */
+    public String process(String msg) {
+        if (!isAuthenticated()) 
+            return authenticate(msg);
+ 
+        String[] msg_parts = msg.split(" ", 3);
+        String comando = msg_parts[0];
+ 
+        if(comando.equals("MENSAJE")) {
+            if(msg_parts.length < 3)
+                return msg_INVALID;
+            String contacto_a_enviar = msg_parts[1];
+            String mensaje = msg_parts[2];
+            if(sendMsg(contacto_a_enviar, mensaje))
+                return msg_OK;
+            else return msg_SEND_FAILED;
+        }
+        else if(comando.equals("ARCHIVO")){
+            if(msg_parts.length < 3)
+                return msg_INVALID;
+            String contacto_a_enviar = msg_parts[1];
+            String nombre_archivo = msg_parts[2];
+            if(sendFile(contacto_a_enviar, nombre_archivo))
+                return msg_FILE_OK;
+            else return msg_SEND_FAILED;
+        }
+        else {
+            return msg_INVALID;
+        }
+    }
+}
--- ServerApplication/src/serverapplication/ServerApplication.java
+++ ServerApplication/src/serverapplication/ServerApplication.java
@@ -11,100 +11,75 @@ package serverapplication;
  * @author Victor
  */
 
+import java.net.*; 
 import java.io.*;
-import java.net.*;
-import java.lang.*;
-import java.util.*;
-
+ 
 public class ServerApplication {
-
-    private ServerSocket server1;
-    private ServerSocket server2;
-    private int port1 = 8090;
-    private int port2 = 8091;
+    private static int port = 1001; /* Puerto a ser escuchado */
  
-    public ServerApplication() {
+    public static void main (String[] args) throws IOException {
+ 
+        ServerSocket server = null;
         try {
-            server1 = new ServerSocket(port1);
-            server2 = new ServerSocket(port2);
+            server = new ServerSocket(port); /* start listening on the port */
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("Puerto no encontrado: " + port);
+            System.err.println(e);
+            System.exit(1);
         }
-    }
  
-    public static void main(String[] args) {
-        ServerApplication example = new ServerApplication();
-        example.handleConnection();
-    }
- 
-    public void handleConnection() {
-        System.out.println("Esperando respuesta del cliente...");
-        
-        //Loop donde se espera respuesta del cliente.
-
-        while (true) {
+        Socket client = null;
+        while(true) {
             try {
-                Socket socket1 = server1.accept();
-                Socket socket2 = server2.accept();
-                new ConnectionHandler(socket1);
-                new ConnectionHandler(socket2);
+                client = server.accept();
             } catch (IOException e) {
-                e.printStackTrace();
+                System.err.println("Accept failed.");
+                System.err.println(e);
+                System.exit(1);
             }
+            /* Thread para escuchar mmúltiples clientes */
+            Thread t = new Thread(new ClientConn(client));
+            t.start();
         }
     }
 }
  
-class ConnectionHandler implements Runnable {
-    private Socket socket;
+class ClientConn implements Runnable {
+    private Socket client;
+    private BufferedReader in = null;
+    private PrintWriter out = null;
  
-    public ConnectionHandler(Socket socket) {
-        this.socket = socket;
- 
-        Thread t = new Thread(this);
-        t.start();
+    ClientConn(Socket client) {
+        this.client = client;
+        try {
+            /* obtain an input stream to this client ... */
+            in = new BufferedReader(new InputStreamReader(
+                        client.getInputStream()));
+            /* ... and an output stream to the same client */
+            out = new PrintWriter(client.getOutputStream(), true);
+        } catch (IOException e) {
+            System.err.println(e);
+            return;
+        }
     }
  
     public void run() {
-        try
-        {
-            //Lee el mensaje enviado por el cliente.
-            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-            String message = (String) ois.readObject();
-            System.out.println("Mensaje recibido: " + message);
-            
-            //Variable para escribir el mensaje de respuesta al cliente.
-            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-            
-            //Reconocimiento de comandos especiales (protocolo)
-            
-            //Separando los parametros del mensaje.
-            StringTokenizer token = new StringTokenizer(message);
-            String comando = token.nextToken();
-            String mensaje = token.nextToken();
-            String IP = token.nextToken();
-            String contacto_a_enviar = token.nextToken();
-            
-            switch (comando) {
-                case "MENSAJE":
-                    break;
-                case "ARCHIVO":
-                    break;
+        String msg, response;
+        Protocolo protocol = new Protocolo(this);
+        try {
+            /* loop reading lines from the client which are processed 
+             * according to our protocol and the resulting response is 
+             * sent back to the client */
+            while ((msg = in.readLine()) != null) {
+                response = protocol.process(msg);
+                out.println("SERVIDOR: " + response);
             }
- 
-            //Envía el mensaje de respuesta al cliente.
-            oos.writeObject("Hi...");
- 
-            ois.close();
-            oos.close();
-            socket.close();
- 
-            System.out.println("Esperando respuesta del cliente...");
         } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            System.err.println(e);
         }
     }
-    
+ 
+    public void sendMsg(String msg) {
+        out.println(msg);
+    }
 }
